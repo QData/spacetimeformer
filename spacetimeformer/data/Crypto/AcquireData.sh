5bcda65dba5f4f7a369a697864eb0dac5871d12a
ferdinandl007 <ferdinandloesch@me.com>
2022-04-27 15:19:17 +0800

    up

--- spacetimeformer/data/Crypto/AcquireData.sh
+++ spacetimeformer/data/Crypto/AcquireData.sh
@@ -1,9 +1,20 @@
 
 #!/bin/sh
 echo "acquiring up-to-date data from Binance"
-rm Binance_ETHUSDT_minute.csv
-rm Binance_BTCUSDT_minute.csv
-wget https://www.cryptodatadownload.com/cdd/Binance_BTCUSDT_minute.csv https://www.cryptodatadownload.com/cdd/Binance_ETHUSDT_minute.csv --no-check-certificate
-sed -i '1d' Binance_ETHUSDT_minute.csv
-sed -i '1d' Binance_BTCUSDT_minute.csv
+!rm BTCUSDT_Binance_futures_data_hour.csv
+!rm ETHUSDT_Binance_futures_data_hour.csv
+!rm LINKUSDT_Binance_futures_data_hour.csv
+!rm EOSUSDT_Binance_futures_data_hour.csv
+!rm XMRUSDT_Binance_futures_data_hour.csv
+!wget https://www.cryptodatadownload.com/cdd/BTCUSDT_Binance_futures_data_hour.csv \
+https://www.cryptodatadownload.com/cdd/ETHUSDT_Binance_futures_data_hour.csv \
+https://www.cryptodatadownload.com/cdd/LINKUSDT_Binance_futures_data_hour.csv \
+https://www.cryptodatadownload.com/cdd/EOSUSDT_Binance_futures_data_hour.csv \
+https://www.cryptodatadownload.com/cdd/XMRUSDT_Binance_futures_data_hour.csv \
+--no-check-certificate
+!sed -i '1d' BTCUSDT_Binance_futures_data_hour.csv
+!sed -i '1d' ETHUSDT_Binance_futures_data_hour.csv
+!sed -i '1d' LINKUSDT_Binance_futures_data_hour.csv
+!sed -i '1d' EOSUSDT_Binance_futures_data_hour.csv
+!sed -i '1d' XMRUSDT_Binance_futures_data_hour.csv
 python ConvertData.py
